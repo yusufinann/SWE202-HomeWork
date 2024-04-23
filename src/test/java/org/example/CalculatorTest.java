cb5e04eebbfd71428312651062d50589702d72de
Yusuf İnan <iinan@DESKTOP-M1F4IS3>
2024-04-23 18:19:05 +0300

    Initial commit

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,52 @@
+//B211202070
+//Yusuf İnan
+//Software validation and verification
+//https://github.com/yusufinann/SWE202-HomeWork
+
+
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "10, 2, 5",
+            "10, 4, 2.5",
+            "12.5, 2.5, 5",
+            "10, 2.5, 4",
+            "12.5, 5, 2.5"
+    })
+    void testDivisionParameterized(float dividend, float divisor, float expectedResult) {
+        assertEquals(expectedResult, Calculator.divide(dividend, divisor));
+    }
+
+    @Test
+    void testDivision6() {
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> Calculator.divide(12.5f, 0),
+                "IllegalArgumentException expected."
+        );
+
+
+        assertEquals("Illegal Argument Exception.", exception.getMessage());
+    }
+}
